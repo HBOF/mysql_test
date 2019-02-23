1a14aba3203831baa780d1cccb3d7b2bdc9da58b
Спасибо <ty662@naver.com>
2019-02-23 17:19:14 +0900

    ㅇㅇ
    ㅇ

--- node_mysql.js
+++ node_mysql.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host    :'192.168.0.58',
     port : 3306,
-    user : 'root',
-    password : 'dsdata1128!',
+    user : 'user',
+    password : 'pass',
     database:'nadobang'
 });
  
